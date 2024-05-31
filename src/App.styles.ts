888aa6059e404be2e1105aa76bd6b0449ebceb96
Fernando Cesar <cmnando.silva@gmail.com>
2024-05-31 12:35:34 -0300

    Feat: Tela inicial feita

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -9,6 +9,9 @@ export const ContainerInitialScreen = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    display: flex; /* Habilita flexbox */
+    justify-content: center; /* Centraliza horizontalmente */
+    align-items: center; /* Centraliza verticalmente */
 
     &::before {
         content: '';
@@ -27,3 +30,32 @@ export const ContainerInitialScreen = styled.div`
         z-index: 2;
     }
 `;
+
+export const ContentInitialScreen = styled.div`
+    z-index: 3;
+    display: flex; /* Habilita flexbox para o conteúdo */
+    justify-content: center; /* Centraliza o conteúdo horizontalmente */
+    align-items: center; /* Centraliza o conteúdo verticalmente */
+    display: flex;
+    flex-direction: column;
+
+    img {
+        width: 30%;
+        height: 15%;
+    }
+`;
+
+export const ButtonInitialScreen = styled.button`
+    margin-top: 5vh;
+    background-color: transparent;
+    border: 2px solid white; /* Borda sólida branca */
+    color: white;
+    font-size: 15px;
+    font-weight: 500;
+    height: 50px;
+    width: 50vw;
+    cursor: pointer;
+`;
+
+
+
--- src/routes/InitialScreen.tsx
+++ src/routes/InitialScreen.tsx
@@ -1,8 +1,18 @@
 import * as CSS from '../App.styles';
+import logotype from '../assets/pngegg.png'
 
 function InitialScreen() {
   return (
     <CSS.ContainerInitialScreen>
+      <CSS.ContentInitialScreen>
+        <img src={logotype} alt="logotype" />
+        <CSS.ButtonInitialScreen>
+          Entrar
+        </CSS.ButtonInitialScreen>
+        <CSS.ButtonInitialScreen>
+          Cadastrar-se
+        </CSS.ButtonInitialScreen>
+      </CSS.ContentInitialScreen>
     </CSS.ContainerInitialScreen>
   );
 }
